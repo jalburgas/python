2b58e81040871a8e5fbfd48b1d8c3b2d08f9e49b
Juan Alburgas <111477971+jalburgas@users.noreply.github.com>
2026-04-20 14:00:53 -0400

    Update 0.py

--- class/0.py
+++ class/0.py
@@ -1,9 +1,17 @@
-# Es unja plantilla para crear objetos de forma predefinida
+# Es una plantilla para crear objetos de forma predefinida
 #permiten representar objetos o entidaes
 class Auto:
     marca = "FIAT"
     modelo = 1977
     placa = "A23-555"
 #creamos el objeto taxi
+#un objeto es un elemento de la vida real
 taxi = Auto()
 print(taxi.marca, taxi.modelo, taxi.placa)
+# creamos el objeto patrulla con diferentes modelo, placa y marca
+patrulla = Auto()
+patrulla.marca = "FORD"
+patrulla.modelo = 2022
+patrulla.placa = "P-911-X"
+print(patrulla.marca, patrulla.modelo, patrulla.placa)
+
