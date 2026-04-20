17e122709ac75389fa1137f43ca13ebfdd43a0ef
Juan Alburgas <111477971+jalburgas@users.noreply.github.com>
2026-04-20 14:05:25 -0400

    Refactor Auto class and instantiate taxi object
    Refactor Auto class attributes to default values and instantiate taxi with specific values.

--- class/0.py
+++ class/0.py
@@ -1,12 +1,15 @@
 # Es una plantilla para crear objetos de forma predefinida
 #permiten representar objetos o entidaes
 class Auto:
-    marca = "FIAT"
-    modelo = 1977
-    placa = "A23-555"
+    marca = ""
+    modelo = 0
+    placa = ""
 #creamos el objeto taxi
 #un objeto es un elemento de la vida real
 taxi = Auto()
+taxi.marca = "FIAT"
+taxi.modelo = 1977
+taxi.placa = "A23-555"
 print(taxi.marca, taxi.modelo, taxi.placa)
 # creamos el objeto patrulla con diferentes modelo, placa y marca
 patrulla = Auto()
